33b08e4a1e5d1b91c5e1ae0bad073e35e3a3ef61
TechDev-CEO <shoriful521399@gmail.com>
2022-08-22 10:10:58 +0600

    Frotend Complete and data fetch, search complete

--- js/mealdb_api.js
+++ js/mealdb_api.js
@@ -1,6 +1,29 @@
+
+
 const serarchFood = () =>{
     const getSearchValue= document.getElementById('search');
     const searchData = getSearchValue.value;
     console.log(searchData);
-    
+    getSearchValue.value = '';
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchData}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=> displayMeals(data.meals))
+}
+const displayMeals = meals =>{
+    const showMeals = document.getElementById('meals-section')
+    showMeals.innerHTML = '';
+    meals.forEach(meal=>{
+        console.log(meal);
+        const {strMeal, strMealThumb} = meal;
+        const div = document.createElement('div');
+        div.classList.add('col-md-3');
+        div.innerHTML = `
+        <div class="meal-info text-center">
+        <img src="${strMealThumb}" class="img-fluid" alt="">
+        <p class="mt-2"><a href="" class="text-decoration-none text-white text-center">${strMeal}</a><p>
+         </div>
+        `
+        showMeals.appendChild(div);
+    })
 }
